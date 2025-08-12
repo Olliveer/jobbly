4617d7986309c1a9a75498e3e673de6696d7b53c
Olliveer <j.oliveira90@live.com>
2025-08-12 20:45:38 -0300

    feat: add shadcn and initial sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
+import { AppSidebarClient } from "@/components/app-sidebar-client";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
 export default function Home() {
-  return <h1>hi</h1>;
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">Jobbly</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem></SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">dasdas</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
